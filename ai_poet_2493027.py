099aae0e6dbf67f57eaf4393f8622500c46b79af
hoyeon0131 <lofld02@gmail.com>
2024-12-08 21:01:10 +0900

    Add AI Poet script

---
+++ ai_poet_2493027.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fc208cdd-d457-4c7a-a34a-f6e823e6e92f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
